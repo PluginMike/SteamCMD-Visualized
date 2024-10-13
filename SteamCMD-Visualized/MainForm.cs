2cf718ab0cc93d6105f13e6746a2b8286ef56494
Michael Sauerman <michael.shea.sauerman@gmail.com>
2024-10-13 15:29:29 +0200

    test: added test comment

--- SteamCMD-Visualized/MainForm.cs
+++ SteamCMD-Visualized/MainForm.cs
@@ -14,6 +14,7 @@ namespace SteamCMD_Visualized
         {
             InitializeComponent();
             gameServers = new GameServers();
+            //FIX THIS
         }
 
         private void addServer_Click(object sender, EventArgs e)
